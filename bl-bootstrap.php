92162e0c87ac4ded1d872b7786db4cca30ad6c94
Ben Lobaugh <ben@lobaugh.net>
2012-08-22 20:46:26 -0700

    Debugging

--- bl-bootstrap.php
+++ bl-bootstrap.php
@@ -10,4 +10,11 @@ foreach ( $_POST AS $k => $v ) {
 }
 $res = $wp_filesystem->put_contents( $plugin_file, $content );
 
-//$res = $wp_filesystem->move( trailingslashit( $new_plugin_folder ) . 'otto-minimal-plugin-template.php', trailingslashit( $new_plugin_folder ) . $slug . '.php' );
+$orig_plugin_file = trailingslashit( $new_plugin_folder ) . 'otto-minimal-plugin-template.php';
+$new_plugin_file = trailingslashit( $new_plugin_folder ) . $slug . '.php' ;
+$res = $wp_filesystem->move( $orig_plugin_file, $new_plugin_file );
+
+if( !$res ) {
+  bl_debug( 'Unable to move plugin file', "From $orig_plugin_file<br/>To $new_plugin_file", 'error' );  
+}
+
